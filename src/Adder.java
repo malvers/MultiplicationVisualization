d3bdca9aa1da643df4c5890b123ce76cc3ddd05f
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2023-11-23 17:15:12 +0100

    just a save

--- src/Adder.java
+++ src/Adder.java
@@ -23,7 +23,7 @@ public class Adder {
         toWrite = "";
         allCarryOver.clear();
 
-        ArrayList<String> linesLocal = new ArrayList<>();
+        ArrayList<String> columns = new ArrayList<>();
         int num0right = lines.size() - 1;
 
         for (int i = 0; i < lines.size(); i++) {
@@ -47,14 +47,17 @@ public class Adder {
 
             theLine = new StringBuilder(tmp.toString());
 
-            linesLocal.add(theLine.toString());
+            columns.add(theLine.toString());
+
         }
 
+        printColumns(columns);
+
         int carryOver = 0;
-        for (int i = linesLocal.get(0).length() - 1; i >= 0; i--) {
+        for (int i = columns.get(0).length() - 1; i >= 0; i--) {
 
             int sum = 0;
-            sum = getSumOneColumn(linesLocal, i, sum);
+            sum = getSumOneColumn(columns, i, sum);
 
             int toWrite;
             if (sum + carryOver >= 10) {
@@ -70,15 +73,28 @@ public class Adder {
 
         Collections.reverse(allCarryOver);
 
+        if (allCarryOver.size() >= 8) {
+            allCarryOver.remove(0);
+        }
+
         StringBuilder reversedStringBuilder = new StringBuilder(result.toString()).reverse();
         result = reversedStringBuilder.toString();
 
-        if( result.substring(0,1).contains("0"))
+        if (result.substring(0, 1).contains("0")) {
             result = result.substring(1);
-        System.out.println("result: " + result);
+        }
 
         numbersToWrite = result.length();
         return Integer.parseInt(result.toString());
+    }
+
+    private void printColumns(ArrayList<String> columns) {
+        System.out.println("Columns printing");
+        for (int i = 0; i < columns.size(); i++) {
+            columns.get(i);
+            System.out.println(columns.get(i));
+        }
+        System.out.println("Columns done");
     }
 
     protected String oneStep() {
@@ -93,21 +109,17 @@ public class Adder {
 
     private int getSumOneColumn(ArrayList<String> linesLocal, int columNum, int sum) {
 
-
         for (int i = linesLocal.size() - 1; i >= 0; i--) {
 
             int digit = Character.getNumericValue(linesLocal.get(i).charAt(columNum));
             sum += digit;
             numbersToAdd.add(sum);
-//            System.out.println("digit: " + digit + " sum: " + sum);
         }
         if (sum >= 10) {
             allCarryOver.add(sum / 10);
         } else {
             allCarryOver.add(0);
         }
-
-        System.out.println("getSumOneColumn: " + allCarryOver.size());
         return sum;
     }
 
@@ -116,26 +128,40 @@ public class Adder {
         Font font = g2d.getFont();
         FontMetrics fontMetrics = g2d.getFontMetrics(font);
 
-        System.out.println("result: " + result.toString());
-
         int lengthResult = fontMetrics.stringWidth(result.toString());
         int lengthToWrite = fontMetrics.stringWidth(toWrite);
 
         g2d.drawString(toWrite, xPos + lengthResult - lengthToWrite, yPos + font.getSize());
 
-//        System.out.println("numbersToWrite: " + (numbersToWrite - 1));
+        printAllCarryOvers();
 
         if (numbersToWrite - 1 < 0) {
             return;
         }
-        int carryOver = allCarryOver.get(numbersToWrite - 1);
 
 //        if (carryOver < 1) {
 //            return;
 //        }
         g2d.setFont(carryOverFont);
-        String carryOverStr = carryOver + "";
-        g2d.drawString(carryOverStr, xPos + lengthResult - 6 - lengthToWrite, yPos);
+
+        int myXPos = xPos + lengthResult - 30;
+        for (int i = 0; i < allCarryOver.size(); i++) {
+            int carryOver = allCarryOver.get(i);
+            String carryOverStr = carryOver + "";
+            g2d.drawString(carryOverStr, myXPos, yPos - 4);
+            myXPos -= 44;
+        }
+
         g2d.setFont(font);
+    }
+
+    private void printAllCarryOvers() {
+        System.out.println("all carry");
+        for (int i = 0; i < allCarryOver.size(); i++) {
+
+            int carryOver = allCarryOver.get(i);
+            System.out.print(carryOver);
+        }
+        System.out.println("\nall carry done");
     }
 }
